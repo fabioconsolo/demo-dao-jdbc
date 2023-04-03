d696abe0bc91d8333f0e61529ffa1c2a2c51c870
Fabio Matarazzo <fabio.consolo@yahoo.com.br>
2023-04-03 11:33:29 -0300

    Department and SellerDao interfaces

---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,19 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entitites.Seller;
+
+public interface SellerDao {
+	
+	
+	void insert(Seller obj);
+	void update(Seller obj);
+	void deleteByid(Integer id);
+	Seller findByid(Integer id);
+	List<Seller> findAll();
+	
+	
+
+
+}
